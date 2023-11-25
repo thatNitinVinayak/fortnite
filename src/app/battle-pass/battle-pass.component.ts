aa48e00c1040a772fb2818e9243f961b08e5d38b
thatnitinvinayak <thatnitinvinayak@icloud.com>
2023-11-25 21:30:10 +0530

    created: BattlePassComponent

---
+++ src/app/battle-pass/battle-pass.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-battle-pass',
+  templateUrl: './battle-pass.component.html',
+  styleUrls: ['./battle-pass.component.scss']
+})
+export class BattlePassComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
