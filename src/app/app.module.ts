35f7ef5471773c6d25e7df3d67ef8d7348da8cbf
thatnitinvinayak <thatnitinvinayak@icloud.com>
2023-11-29 20:18:24 +0530

    modified: AppComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,27 +4,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
-import { CurrentSeasonComponent } from './current-season/current-season.component';
+import { FortniteSeasonComponent } from './fortnite-season/fortnite-season.component';
 import { BattlePassComponent } from './battle-pass/battle-pass.component';
 import { FortniteMapComponent } from './fortnite-map/fortnite-map.component';
 import { SearchFnComponent } from './search-fn/search-fn.component';
-import { FortniteSeasonComponent } from './fortnite-season/fortnite-season.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent,
-    CurrentSeasonComponent,
-    BattlePassComponent,
-    FortniteMapComponent,
-    SearchFnComponent,
-    FortniteSeasonComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        HomeComponent,
+        FortniteSeasonComponent,
+        BattlePassComponent,
+        FortniteMapComponent,
+        SearchFnComponent,
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule
+    ],
+    providers: [],
+    bootstrap: [
+        AppComponent
+    ]
 })
+
 export class AppModule { }
