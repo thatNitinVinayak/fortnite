b6f9d22fd7f5bd342b7e73f8248e4884cc0c1acf
thatnitinvinayak <thatnitinvinayak@icloud.com>
2024-01-27 17:27:22 +0530

    modified: AppComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,4 @@
 import { Component } from '@angular/core';
-import { MenuService } from './service/menu.service';
-import { IMenuItems } from './interface/menu';
 
 @Component({
 		selector: 'app-root',
@@ -10,16 +8,5 @@ import { IMenuItems } from './interface/menu';
 
 export class AppComponent {
 	title = 'FORTNITE';
-
-	public menuItems: any = '';
-	public menuSelected: string = 'home';
-
-	constructor(private menuService: MenuService) {
-		this.menuItems = this.menuService.menuItems;
-	}
-
-	public menuItemSelected(menuItem: string) {
-		this.menuSelected = menuItem;
-
-	}
+	constructor() {}
 }
